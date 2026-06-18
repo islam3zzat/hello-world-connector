73578e37e7827f5c5f60e6b1b6bfa1cb22753206
Islam farg <islam.farg@commercetools.com>
2026-06-18 10:39:37 +0100

    remove service

--- service/src/interfaces/response.interface.ts
+++
@@ -1,9 +0,0 @@
-import { CartUpdateAction } from '@commercetools/platform-sdk';
-
-export interface ResponseInterfaceSuccess {
-  actions: Array<CartUpdateAction>;
-}
-
-export interface ResponseInterfaceError {
-  errors: Array<unknown>;
-}
